8bfa1fa6f450890dd9ecb9b5c4699aa608ece9ae
Jxkeorton <jakeorton99@gmail.com>
2025-02-19 21:35:56 +0000

    Clean up

--- test.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const {updateCameraInvoice} = require('./spreadsheets/updateCameraInvoice')
-
-const invoiceSpreadsheetId = process.env.INVOICE_SPREADSHEET_ID;
-
-updateCameraInvoice(invoiceSpreadsheetId, 1);
